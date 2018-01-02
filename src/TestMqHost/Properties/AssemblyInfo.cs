4486afbbe8ff01dce8115bfd3f971dac76c97601
申行 <binary2015nov@outlook.com>
2018-01-02 16:08:40 +0800

    update version

--- src/TestMqHost/Properties/AssemblyInfo.cs
+++ src/TestMqHost/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.5.9.0")]
-[assembly: AssemblyFileVersion("4.5.9.0")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
